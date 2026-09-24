3fc06335e4581de794b950aa8c5fd515e4c9464d
yulcaribe <mehmetisaacar47@gmail.com>
2026-09-25 00:37:26 +0300

    Disable search engine indexing

--- health.php
+++ health.php
@@ -1,5 +1,6 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
+header('X-Robots-Tag: noindex, nofollow, noarchive, nosnippet');
 echo json_encode([
     'ok' => true,
     'service' => 'ocr',
